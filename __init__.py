543e4887ac25c928cf35866e2ca6ccae8311b06a
ercas <edgarrimandocastro@gmail.com>
2018-06-17 22:20:16 -0400

    lazy loading of indices

--- __init__.py
+++ __init__.py
@@ -4,24 +4,43 @@ import os
 
 from .shape_index import ShapeIndex
 
-indices = {}
+class LazyLoadIndices(object):
 
-shapes_dir = os.path.join(
-    os.path.dirname(
-        os.path.realpath(__file__)
-    ),
-    "shapes"
-)
-
-for geojson in os.listdir(shapes_dir):
-    index = ShapeIndex()
-    index.add_geojson(os.path.join(shapes_dir, geojson))
-    indices[".".join(geojson.split(".")[:-1])] = index
-
-def list_indices():
-    print("\n".join([
-        "%s: %d shape%s" % (
-            index_name, index.n_shapes, index.n_shapes == 1 and " " or "s"
+    def __init__(self):
+        self.indices = {}
+        self._shapes_dir = os.path.join(
+            os.path.dirname(
+                os.path.realpath(__file__)
+            ),
+            "shapes"
         )
-        for (index_name, index) in indices.items()
-    ]))
+        self._index_names = [
+            ".".join(geojson.split(".")[:-1])
+            for geojson in os.listdir(self._shapes_dir)
+        ]
+
+    def list(self):
+        for index_name in self._index_names:
+            if (index_name in self.indices):
+                index = self.indices[index_name]
+                print("%s: %d shape%s" % (
+                    index_name, index.n_shapes, index.n_shapes == 1 and " " or "s"
+                ))
+            else:
+                print("%s: uninitialized" % index_name)
+
+    def __getitem__(self, key):
+        if (not key in self._index_names):
+            raise KeyError("no such index")
+        elif (key in self.indices):
+            return self.indices[key]
+        else:
+            index = shape_index.ShapeIndex()
+            index.add_geojson(os.path.join(
+                self._shapes_dir,
+                "%s.geojson" % key
+            ))
+            self.indices[key] = index
+            return index
+
+indices = LazyLoadIndices()
